ee377b71d089b29c28ad8f8153c4443e44efbb5e
fldnnn <fuldenonan@gmail.com>
2022-12-08 23:15:04 +0300

    Initial Commit

---
+++ CovidInfo/CovidInfo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CovidInfo
+//
+//  Created by Fulden Onan on 8.12.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
